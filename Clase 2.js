e32436ce74479819321cd4f68377759ebfeab2d6
javier <javier.alas05@gmail.com>
2021-05-25 13:22:46 -0600

    Clase 2 actualizada con desafio para mostrar la ultima letra del nombre del usuario

--- Clase 2.js
+++ Clase 2.js
@@ -1,4 +1,4 @@
-var nombre = 'Rodrigo', apellido = 'Alas'
+var nombre = 'Susan', apellido = 'Alas'
 
 var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinisculas = apellido.toLowerCase()
@@ -8,6 +8,23 @@ var cantidadDeLetrasDelNombre = nombre.length
 
 var nombreCompleto =  `${nombre} ${apellido.toUpperCase()}`
 
-console.log(nombreCompleto) 
+var str = nombre.charAt(nombre.length-1)
+
+var string = nombre.substr(0, 1)
+
+
+
+var userName ;
+    userName = prompt("What is your name?");
+
+    console.log("las Letter Of Name" + userName.charAt(userName.length -1))
+
+    document.write("la ultima letra letra de su nombre es" + ' ' + userName.charAt(userName.length -1))
+
+
+
+
+
+
 
 
