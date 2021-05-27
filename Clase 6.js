26865bbd46fcb6880c9605db6f69a5bba14ca85d
javier <javier.alas05@gmail.com>
2021-05-26 23:56:57 -0600

    Clase 6, objetos

---
+++ Clase 6.js
@@ -0,0 +1,18 @@
+var javier= {
+    nombre: 'Javier',
+    edad: 27
+}
+
+var rodrigo= {
+    nombre: 'Rodrigo',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    var nombre = persona.nombre.toUpperCase()
+    console.log(nombre)
+   };
+   
+   imprimirNombreEnMayusculas(javier)
+   imprimirNombreEnMayusculas(rodrigo) 
+   
